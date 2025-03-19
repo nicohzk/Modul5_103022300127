5464350bcae01807ec9340be86a2e39b9559fb7b
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-19 07:12:41 +0700

    menambahkan class PemrosesData dengan method DapatkanNilaiTerbesar.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime;
+
+class PemrosesData {
+
+    public T DapatkanNilaiTerbesar<T>(T inp,T inp2,T inp3) {
+        dynamic temp1 = (dynamic)inp;
+        dynamic temp2 = (dynamic)inp2;
+        dynamic temp3 = (dynamic)inp3;
+
+        dynamic? Largest = temp1;
+        if (Largest < temp2) {
+            Largest = temp2;
+        }
+        if(Largest < temp3) {
+            Largest = temp3;
+        }
+        return Largest;
+
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        PemrosesData data = new PemrosesData();
+        int x = data.DapatkanNilaiTerbesar<int>(10, 11, 12);
+        Console.WriteLine(x);
+    }
+}
--- obj/Debug/net8.0/modul5_103022300127.AssemblyInfo.cs
+++ obj/Debug/net8.0/modul5_103022300127.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("modul5_103022300127")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b5c7e9e3d8839afdc5e61e7be2e58b996386013")]
 [assembly: System.Reflection.AssemblyProductAttribute("modul5_103022300127")]
 [assembly: System.Reflection.AssemblyTitleAttribute("modul5_103022300127")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
